4868ae06cfc618ab80f5028f9471441563b291c0
Matthew Goslett <matthew@goslett.co.za>
2019-05-30 11:07:13 +0200

    fix handling of default date values

--- src/Components/DateInput.php
+++ src/Components/DateInput.php
@@ -2,6 +2,9 @@
 
 namespace Balfour\LaravelFormBuilder\Components;
 
+use Carbon\CarbonInterface;
+use Illuminate\Support\Carbon;
+
 class DateInput extends TextInput
 {
     /**
@@ -13,6 +16,28 @@ class DateInput extends TextInput
      * @var string
      */
     protected $type = 'date';
+
+    /**
+     * @param mixed $default
+     * @return $this
+     */
+    public function defaults($default)
+    {
+        if (!is_callable($default)) {
+            if ($default instanceof CarbonInterface) {
+                $default = $default->toDateString();
+            } else {
+                // laravel casts 'date' attributes to date time in the format "YYYY-MM-DD H:i:s"
+                // the date input expects "YYYY-MM-DD", so we re-format here
+                $date = Carbon::parse($default);
+                $default = $date->toDateString();
+            }
+        }
+
+        $this->default = $default;
+
+        return $this;
+    }
 
     /**
      * @return array
