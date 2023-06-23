9c93417ca63e2a0a6e681d04ff819794a710f47b
Nic Appleby <nicja@users.noreply.github.com>
2023-06-23 13:20:30 +0200

    respect disbaled flag (#4)

--- src/views/switch.blade.php
+++ src/views/switch.blade.php
@@ -3,7 +3,7 @@
 @endif
 
 <label class="switch switch-label switch-pill switch-success">
-    <input type="checkbox" name="{{ $name }}" value="1" class="switch-input" {{ $isOn ? 'checked' : '' }}>
+    <input type="checkbox" name="{{ $name }}" value="1" class="switch-input" {{ $isOn ? 'checked' : '' }} {{ $disabled ? 'disabled' : '' }}>
     <span class="switch-slider {{ implode(' ', $classes) }}" data-checked="{{ $onLabel ?? 'On' }}" data-unchecked="{{ $offLabel ?? 'Off' }}"></span>
 </label>
 
